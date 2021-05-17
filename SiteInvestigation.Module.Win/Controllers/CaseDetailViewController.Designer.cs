3d2f509b1b27d9f503accef9e3479ed66fe8f4bf
苏鹏 <supeng@kingsware.cn>
2021-05-17 08:25:46 +0800

    sample

---
+++ SiteInvestigation.Module.Win/Controllers/CaseDetailViewController.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace SiteInvestigation.Module.Win.Controllers
+{
+    partial class CaseDetailViewController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.createDNA = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // createDNA
+            // 
+            this.createDNA.Caption = "新建DNA取证";
+            this.createDNA.Category = "Edit";
+            this.createDNA.ConfirmationMessage = null;
+            this.createDNA.Id = "9bff900a-cf17-4132-a933-e5a0e915e827";
+            this.createDNA.TargetObjectType = typeof(SiteInvestigation.Module.BusinessObjects.DNA);
+            this.createDNA.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
+            this.createDNA.ToolTip = null;
+            this.createDNA.TypeOfView = typeof(DevExpress.ExpressApp.DetailView);
+            this.createDNA.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.createDNA_Execute);
+            // 
+            // CaseDetailViewController
+            // 
+            this.Actions.Add(this.createDNA);
+            this.TargetObjectType = typeof(SiteInvestigation.Module.BusinessObjects.DNA);
+            this.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction createDNA;
+    }
+}
---
+++ SiteInvestigation.Module.Win/Forms/FrmDNACreation.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace SiteInvestigation.Module.Win.Forms
+{
+    partial class FrmDNACreation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.galleryControl1 = new DevExpress.XtraBars.Ribbon.GalleryControl();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.galleryControlClient1 = new DevExpress.XtraBars.Ribbon.GalleryControlClient();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.galleryControl1)).BeginInit();
+            this.galleryControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.galleryControl1);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(800, 450);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(800, 450);
+            this.Root.TextVisible = false;
+            // 
+            // galleryControl1
+            // 
+            this.galleryControl1.Controls.Add(this.galleryControlClient1);
+            this.galleryControl1.Location = new System.Drawing.Point(12, 12);
+            this.galleryControl1.Name = "galleryControl1";
+            this.galleryControl1.Size = new System.Drawing.Size(776, 426);
+            this.galleryControl1.StyleController = this.layoutControl1;
+            this.galleryControl1.TabIndex = 4;
+            this.galleryControl1.Text = "galleryControl1";
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.galleryControl1;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(780, 430);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // galleryControlClient1
+            // 
+            this.galleryControlClient1.GalleryControl = this.galleryControl1;
+            this.galleryControlClient1.Location = new System.Drawing.Point(2, 2);
+            this.galleryControlClient1.Size = new System.Drawing.Size(751, 422);
+            // 
+            // FrmDNACreation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "FrmDNACreation";
+            this.Text = "FrmDNACreation";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.galleryControl1)).EndInit();
+            this.galleryControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraBars.Ribbon.GalleryControl galleryControl1;
+        private DevExpress.XtraBars.Ribbon.GalleryControlClient galleryControlClient1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+    }
+}
